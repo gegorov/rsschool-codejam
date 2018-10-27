c7c5f5315630594283aaae945e2984231f0e2326
gegorov <gegorov@yahoo.com>
2018-10-28 01:23:45 +0300

    feat: add test for recurstion

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const sumOfOthers = require('../src/sumOfOthers');
 const make = require('../src/make');
+const recursion = require('../src/recursion');
 
 describe('#sumOfOthers()', () => {
   it('sumOfOthers([2, 3, 4, 1]) should return [8, 7, 6, 9]', () => {
@@ -24,5 +25,34 @@ describe('#make()', () => {
   });
   it('make(\'мама\')(\' \')(\'мыла раму\')((a, b) => a + b) should return "мама мыла раму"', () => {
     assert.equal(make('мама')(' ')('мыла раму')((a, b) => a + b), 'мама мыла раму');
+  });
+});
+
+describe('#recursion()', () => {
+  it('{"value":100,"left":{"value":90,"left":{"value":70},"right":{"value":99}},"right":{"value":120,'
+  + '"left":{"value":110},"right":{"value":130}}} should return [[100], [90, 120], [70,99,110,130]]',
+  () => {
+    const tree = {
+      value: 100,
+      left: {
+        value: 90,
+        left: {
+          value: 70,
+        },
+        right: {
+          value: 99,
+        },
+      },
+      right: {
+        value: 120,
+        left: {
+          value: 110,
+        },
+        right: {
+          value: 130,
+        },
+      },
+    };
+    assert.deepEqual(recursion(tree), [[100], [90, 120], [70, 99, 110, 130]]);
   });
 });
